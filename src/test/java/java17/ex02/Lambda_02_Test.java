7cfb5cd523d742b5b31b2c369b8a4acd3884e81a
khora-san <jeremie.fandos@outlook.fr>
2026-07-23 12:22:51 +0200

    tp2-lambda terminé

--- src/test/java/java17/ex02/Lambda_02_Test.java
+++ src/test/java/java17/ex02/Lambda_02_Test.java
@@ -1,10 +1,9 @@
 package java17.ex02;
 
-import org.junit.Test;
-
 import java17.data.Account;
 import java17.data.Data;
 import java17.data.Person;
+import org.junit.Test;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -35,10 +34,7 @@ public class Lambda_02_Test {
     public void test_map_person_to_account() throws Exception {
 
         List<Person> personList = Data.buildPersonList(100);
-
-        // TODO transformer la liste de personnes en liste de comptes
-        // TODO tous les objets comptes ont un solde à 100 par défaut
-        List<Account> result = map(personList, null);
+        List<Account> result = map(personList, p -> new Account(p, 100));
 
         assert result.size() == personList.size();
         for (Account account : result) {
